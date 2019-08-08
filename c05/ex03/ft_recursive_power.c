60b4c8ffc685293cba785a2d81742e86b003826b
Jean-jacques Acens <jacens@z1r7p4.le-101.fr>
2019-08-08 17:12:38 +0200

    norminette c05

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_recursive_power.c                             .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jacens <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/08/08 17:04:27 by jacens       #+#   ##    ##    #+#       */
+/*   Updated: 2019/08/08 17:04:28 by jacens      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 int		ft_recursive_power(int nb, int power)
 {
 	if (power == 0)
 		return (1);
 	else if (nb > 0)
-		return (nb * ft_recursive_power(nb, power - 1);
+		return (nb * ft_recursive_power(nb, power - 1));
 	return (0);
 }
