dab56b6f18d25ee5096d4f8a671c87565d6311ad
Jean-jacques Acens <jacens@z1r7p4.le-101.fr>
2019-08-09 11:21:53 +0200

    c06 ex02

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void	ft_rev_programs(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char **ag)
+{
+	int i;
+
+	i = 0;
+	while (i != ac)
+		i++;
+	while (i != 0)
+	{
+		ft_rev_programs(ag[i]);
+		write(1, " ", 1);
+		i--;
+	}
+	return (0);
+}
