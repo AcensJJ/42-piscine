4bc9ada4cb24fcd46cdaad32e1c389de7bbe71e5
Jean-jacques Acens <jacens@z1r8p5.le-101.fr>
2019-08-13 12:31:48 +0200

    c7

---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int		ft_compte(char **strs)
+{
+	int i;
+
+	i = 0;
+	while (*strs[i] != '\0')
+		i++;
+	return (i);
+}
+
+char 	*ft_strjoin(int size, char **strs, char sep)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	strs = malloc(sizeof(int *) * ();
+	if (size == 0)
+		return (NULL);
+	if (size == 1)
+		return (strs);
+	while (i != size)
+	{
+		i++;
+	}
+}
--- test/test.c
+++ test/test.c
@@ -2,21 +2,28 @@ int		ft_is_prime(int nb)
 {
 	int i;
 
-	i = nb - 1;
+	i = 2;
 	if (nb == 2)
 		return (1);
 	if (nb < 2)
 		return (0);
-	while (i != 2)
-	{
-		if (nb % i == 0)	
-			return (0);
-		i--;
-	}
+	while (nb % i != 0 && (i * i <= nb) && i <= 46340)
+		i++;
+	if (nb % i == 0)
+		return (0);
 	return (1);
 }
 
+int		ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
+
+#include <stdio.h>
+
 int main()
 {
-	printf("%d", ft_is_prime(1000000007));
+	printf("%d", ft_find_next_prime(2147483647));
 }
