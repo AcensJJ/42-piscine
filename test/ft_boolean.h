d32595e1a84c3e16eb869d6d9c24f0d287a0a7c0
Jean-jacques Acens <jacens@z1r12p4.le-101.fr>
2019-08-15 13:49:41 +0200

    c08

---
+++ test/ft_boolean.h
@@ -0,0 +1,14 @@
+#ifndef _FT_BOOLEAN_H
+
+# define _FT_BOOLEAN_H
+# define t_bool int
+# define SUCCESS 0
+# define TRUE 0
+# define FALSE 1
+# define EVEN(nbr) nbr % 2
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# include <unistd.h>
+
+#endif
--- test/test.c
+++ test/test.c
@@ -1,93 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "ft_boolean.h"
 
-int		ft_strlensep(char *sep)
+void	ft_putstr(char *str)
 {
-	int i;
-
-	i = 0;
-	while (sep[i] != '\0')
-		i++;
-	i++;
-	return (i);
+	while (*str)
+		write(1, str++, 1);
 }
 
-int		ft_strlen_total(char **strs, char *sep, int size)
+t_bool	ft_is_even(int nbr)
 {
-	int i;
-	int j;
-	int k;
-
-	i = 0;
-	k = 0;
-	while (i != size)
-	{
-		j = 0;
-		while (strs[i][j] != '\0')
-		{
-			j++;
-			k++;
-		}
-		k++;
-		i++;
-	}
-	k += (size - 1) * ft_strlensep(sep);
-	return (k);
+	return ((EVEN(nbr)) ? TRUE : FALSE);
 }
 
-char	*ft_strcat(char *dest, char *src)
+int		main(int argc, char **argv)
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0')
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	dest[i + j] = '\0';
-	return (dest);
-}
-
-char 	*ft_strjoin(int size, char **strs, char *sep)
-{
-	int i;
-	char *dest;
-
-	dest = NULL;
-	if (size == 0)
-	{
-		dest = malloc(1);
-		dest = 0;
-		return (dest);
-	}
-	dest = malloc(sizeof(char) * ft_strlen_total(strs, sep, size) + 1);
-	if (!strs)
-		return (dest);
-	i = 0;
-	while (i < size)
-	{
-		ft_strcat(dest, strs[i]);
-		if (i != size - 1)
-			ft_strcat(dest, sep);
-		i++;
-	}
-	return (dest);
-}
-
-
-int main()
-{
-	char tutu[] = "tutu";
-	char titi[] = "titi";
-	char toto[] = "toto";
-	char sep[] = "sep";
-	char *strs[3] = {tutu, titi, toto};
-	
-	printf("%s\n", ft_strjoin(3, strs, sep));
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
 }
